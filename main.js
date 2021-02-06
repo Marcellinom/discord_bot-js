db04af0c86271be07c71608725d6ebc77b045425
Marslel <marcellino.janitra@yahoo.com>
2021-02-06 12:42:34 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const discord = require('discord.js')
+const client = new discord.Client()
+
+client.once('ready', ()=>{
+    console.log('logged in!')
+})
+member_to_DM = [
+  '269397446516408331', '515940939257085966', '418177899712086016'
+]
+client.on("message", async (message) => {
+  if (message.author.id == '807462756113842176') return;
+  if (message.author.id == '804604322117189683'){
+    for(var val of member_to_DM){
+      var member = message.guild.members.fetch(val)
+      console.log(val)
+      console.log(member)
+      //let DM users
+      ;(await member).send(message.content)
+    }
+  }
+  });
+client.login('ODA3NDYyNzU2MTEzODQyMTc2.YB4WSg._xCFd2J0FyIEt71CeLlDwqzoJWE')
