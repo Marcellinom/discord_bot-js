c8e7c5558bfb51c5bb5a96f115d5727f652ef8e1
Marslel <marcellino.janitra@yahoo.com>
2021-02-06 21:38:24 +0700

    add tambah notif, hapus notif, show list yang akan dinotifikasi

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const discord = require('discord.js')
 const client = new discord.Client()
-
+const { prefix, token } = require('./config.json');
+var notifDict = {};
 client.once('ready', ()=>{
     console.log('logged in!')
 })
-member_to_DM = [
-  '269397446516408331', '515940939257085966', '418177899712086016'
-]
 client.on("message", async (message) => {
   if (message.author.id == '807462756113842176') return;
   if (message.author.id == '804604322117189683'){
-    for(var val of member_to_DM){
-      var member = message.guild.members.fetch(val)
+    for(var k in notifDict){
+      var member = message.guild.members.fetch(notifDict[k])
       console.log(val)
       console.log(member)
       //DM users
@@ -19,4 +17,69 @@ client.on("message", async (message) => {
     }
   }
   });
-client.login(process.env.TOKEN)
+client.on('message', async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  console.log(args[1])
+  let userToDM = args[1]
+  let arg = args[1]
+  let comTemp = args
+  const command = comTemp.shift().toLowerCase();
+  switch(command){
+    case 'ping':
+      message.channel.send('Pong.');
+      break;
+      case 'beep':
+      message.channel.send(`boob`);
+      break;
+    case 'notif':
+      if (userToDM.startsWith('<@') && userToDM.endsWith('>')) {
+        userToDM = userToDM.slice(2, -1);
+      
+        if (userToDM.startsWith('!')) {
+          userToDM = userToDM.slice(1);
+        }
+        console.log(userToDM)
+          //message.channel.send(`${userToDM}`);
+      }
+      notifDict[userToDM] = userToDM;
+      let man = await message.guild.members.fetch(userToDM)
+      message.channel.send(`${man.user.username} is assigned to be notified`);
+      break;
+    case 'unnotif':
+      if (userToDM.startsWith('<@') && userToDM.endsWith('>')) {
+        userToDM = userToDM.slice(2, -1);
+        
+        if (userToDM.startsWith('!')) {
+          userToDM = userToDM.slice(1);
+        }
+      }
+      const ind = notifDict.indexOf(userToDM);
+        if (ind > -1) {
+          notifDict.splice(ind, 1);
+          let man = await message.guild.members.fetch(userToDM)
+          message.channel.send(`${man.user.username} is removed from notification list!`);
+        } else {
+          message.channel.send(`User doesn't exist!`);
+        }
+      break;
+    case 'list':
+      console.log(arg)
+      if(arg.includes('notif')){
+        var tagged = "";
+        for(var k in notifDict){
+          //message.channel.send(notifDict[k]);
+          var member = await message.guild.members.fetch(notifDict[k])
+          tagged = tagged+`${member.user.username}, `;
+        }
+        if(tagged != ""){
+          message.channel.send(`${tagged} are who will be notified`)
+        } else {
+          message.channel.send("empty")
+        }
+      }
+    break;
+  }    // other commands...
+});
+client.login(process.env.token)
