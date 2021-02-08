9670de5c1599c91664a2bb5f9730bbe03de8e340
Marcellino Mahesa <74979139+Marcellinom@users.noreply.github.com>
2021-02-08 08:13:51 +0700

    undefined fix

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ client.on('message', async (message) => {
                 'https://nhentai-pages-api.herokuapp.com/' + temp_read
                 ); // nh get pict API
                 data_read = await req_read.json();
-                if(typeof data_read['details']['languages'] === 'undefined') continue start_position;
+                if(data_read['status']) continue start_position;
                 console.log(data_read['details']['tags'])
                 if(!data_read['details']['languages'].toString().toLowerCase().includes('english')) continue start_position;
                 break;
@@ -171,7 +171,7 @@ client.on('message', async (message) => {
                 data_read = await req_read.json();
                 i++; if(i>100){  message.channel.send('not found!');return;  }
                 console.log(temp_detail)
-                if(typeof data_read['details']['tags'] === 'undefined') continue start_position;
+                if(data_read['status']) continue start_position;
                 console.log(data_read['details']['tags'])
                 if(!data_read['details']['tags'].toString().toLowerCase().includes(tagArg)) continue start_position;
                 break;
@@ -235,7 +235,7 @@ client.on('message', async (message) => {
               'https://nhentai-pages-api.herokuapp.com/' + temp_detail
               ); // nh get pict API
               data_detail = await req_detail.json();
-              if(typeof data_detail['details']['languages'] === 'undefined') continue start_position;
+              if(data_detail['status']) continue start_position;
               console.log(data_detail['details']['languages'])
               if(!data_detail['details']['languages'].toString().toLowerCase().includes('english')) continue start_position;
               break;
@@ -251,7 +251,7 @@ client.on('message', async (message) => {
               data_detail = await req_detail.json();
               i++; if(i>100){  message.channel.send('not found!');return;  }
               console.log(temp_detail)
-              if(typeof data_detail['details']['tags'] === 'undefined') continue start_position;
+              if(data_detail['status']) continue start_position;
               console.log(data_detail['details']['tags'])
               if(!data_detail['details']['tags'].toString().toLowerCase().includes(tagArg)) continue start_position;
               break;
